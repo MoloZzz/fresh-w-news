7baba0dbb190c0d6bfc5b6cc333ed7d9930026d9
MoloZzz <mega_oleksii@ukr.net>
2025-11-30 03:49:51 +0200

    docs: update swagger IdParamDto

--- api/src/common/dto/id-param-dto.ts
+++ api/src/common/dto/id-param-dto.ts
@@ -1,7 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsDefined, IsNotEmpty, IsString } from 'class-validator';
 
 export class IdParamDto {
+  @ApiProperty({
+    description: 'Unique identifier',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
   @IsDefined()
   @IsString()
   @IsNotEmpty()
