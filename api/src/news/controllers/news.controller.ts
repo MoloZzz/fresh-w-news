02e952c20d880e2e19556f7b19cd8d49ef3bd5da
MoloZzz <mega_oleksii@ukr.net>
2025-12-01 00:34:59 +0200

    fix: small update

--- api/src/news/controllers/news.controller.ts
+++ api/src/news/controllers/news.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Get, Param, Query, Req } from '@nestjs/common';
+import { Controller, Get, Param, Query, Req, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { IdParamDto } from 'src/common/dtos';
 import { FeedOptionsQuery } from '../common/dto/feed-options.query';
 import { NewsService } from '../services/news.service';
+import { OptionalJwtAuthGuard } from 'src/user/guard/optional-jwt.guard';
 
 @ApiTags('News API')
 @Controller('news')
 export class NewsController {
   constructor(private readonly service: NewsService) {}
 
+  @UseGuards(OptionalJwtAuthGuard)
   @Get('feed')
   async fetchNews(@Query() query: FeedOptionsQuery, @Req() req: any) {
-    const user = req.user || null;
+    const user = req.user ? req.user.userId : null;
     return this.service.getFeed(query, user);
   }
 
